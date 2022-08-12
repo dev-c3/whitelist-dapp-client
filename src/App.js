1d9ad8001746e93a934345b1bafe5d57dafaeddc
- <107075687+dev-c3@users.noreply.github.com>
2022-08-12 15:04:12 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  WagmiConfig,
+  createClient,
+  configureChains,
+  chain,
+} from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Main from './pages/Main'
+
+const infuraId = "70217b9a60fd4fe0a5c8900c9d86f871";
+
+
+
+export default function App(){
+const {chains, provider, webSocketProvider} = configureChains(
+  [chain.goerli],
+  [publicProvider()],
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+})
+
+return <WagmiConfig client={client}>
+  <Main/>
+</WagmiConfig>;
 }
 
-export default App;
+
+
